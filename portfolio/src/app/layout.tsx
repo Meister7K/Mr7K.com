a15ae50d035ca260f35800a0dbd5bfbadc00c19f
Meister7K <kmanfinkel17@gmail.com>
2024-06-10 12:55:55 -0600

    'first commit'

---
+++ portfolio/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Mr. 7K Personal Portfolio",
+  description: "The Personal portfolio of Mr. 7K and his interests including, Finance, Investing, Coding, Programming, Sports, & Gaming.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
